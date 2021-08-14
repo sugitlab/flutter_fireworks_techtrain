5748ab1c0b0db0aa72645fe187cb53597945de8e
Sugit <sgmt.snj@gmail.com>
2021-08-14 15:36:39 +0900

    change app name

--- lib/main.dart
+++ lib/main.dart
@@ -11,7 +11,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
+      title: 'お盆de花火',
       theme: ThemeData.dark(),
       home: const MyHomePage(),
     );
